90c8ba763d59cce5a67195380fd500dc720e0f8e
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-01-13 14:34:15 -0800

    Completed Map Practice

--- src/MapPractice.java
+++ src/MapPractice.java
@@ -1,29 +1,52 @@
-
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class MapPractice {
   public static void main(String[] args) {
     // Create a HashMap with String keys and Integer values and
     // assign it to a variable of type Map
+    Map <String, Integer> wordMap = new HashMap <>();
 
     // Put 3 different key/value pairs in the Map
     // (it's OK to do this one-by-one)
+    wordMap.put("Glasgow", 1);
+    wordMap.put("Mexico City", 10);
+    wordMap.put("Madrid", 2);
 
     // Get the value associated with a given key in the Map
+    wordMap.get("Glasgow");
 
     // Find the size (number of key/value pairs) of the Map
+    wordMap.size();
 
     // Replace the value associated with a given key (the size of the Map shoukld not change)
+    wordMap.replace("Mexico City", 20);
 
     // Check whether the Map contains a given key
+    wordMap.containsKey("Madrid");
 
     // Check whether the Map contains a given value
+    wordMap.containsValue(13);
 
     // Iterate over the keys of the Map, printing each key
-
+    for(String key : wordMap.keySet())
+    {
+      System.out.println(key);
+    }
+    System.out.println();
     // Iterate over the values of the map, printing each value
-
+    for(int value : wordMap.values())
+    {
+      System.out.println(value);
+    }
+    System.out.println();
     // Iterate over the entries in the map, printing each key and value
-
+    for(String key : wordMap.keySet())
+    {
+      System.out.println("Key: " + key + " Value: " + wordMap.get(key));
+    }
+    
     /*
      * Usage tip!
      * 
