8afcab5fd75dc157e427af16e1a9130733154334
X BELL <194058603+bellxalli@users.noreply.github.com>
2025-01-13 22:24:27 -0800

    Completed Person Class Practice

--- src/Person.java
+++ src/Person.java
@@ -6,14 +6,33 @@
 public class Person {
   // Declare a public String instance variable for the name of the person
   // Declare a private int instance variable for the age of the person
-
+  public String name;
+  private int age;
 
   // Create a constructor that takes the name and age of the person
   // and assigns it to the instance variables
-
+  public Person(String pName, int pAge)
+  {
+    name = pName;
+    age = pAge;
+  }
 
   // Create a toString method that gives the name and age of the person
+  @Override
+  public String toString()
+  {
+    return "Name: " + name + " Age: " + age;
+  }
 
+  public String getName()
+  {
+    return name;
+  }
+
+  public int getAge()
+  {
+    return age;
+  }
 
   // Implement the below public instance method "birthYear"
   // There should NOT be any print statement in this method.
@@ -28,25 +47,35 @@ public class Person {
    * @return The year the person was born
    */
   // (create the instance method here)
-
+  public int birthYear(int currentYear)
+  {
+    return currentYear - age;
+  }
 
   public static void main(String[] args) {
     // Create an instance of Person
+    Person person1 = new Person("Ivy", 20);
 
     // Create another instance of Person with a different name and age and
     // assign it to a different variable
+    Person person2 = new Person("Kai", 17);
 
     // Print the first person
+    System.out.println(person1);
 
     // Print the second person
+    System.out.println(person2);
 
     // Get the name of the first person and store it in a local variable
+    String pName1 = person1.getName();
 
     // Using the birthYear method, get the birth year of the first person
     // and store it in a local variable. Input the actual current year (e.g. 2025)
     // as the argument.
+    int pBirthYear1 = person1.birthYear(2025);
 
     // In a separate statement, print the local variable holding the birth year.
+    System.out.println(pBirthYear1);
 
     /**
      * Terminology!
